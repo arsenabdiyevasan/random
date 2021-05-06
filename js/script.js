4c69c3984795e702783d191e0fbd98ab608f3409
arsenabdiyevasan <abdievarsen091@gmail.com>
2021-05-06 11:40:44 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,50 @@
 const btn=document.querySelector('.btn')
 const inputs=document.querySelector('.inputs')
 
-btn.onclick=()=>{
-    let prompts=prompt('сколь участников')
-    localStorage.setItem('numbers',prompts)
-    if(prompts <= 1){
-        alert('участников слишком мало их долно быть больше одного')
-    }else{
-        window.open('index2.html','_self')
-    }
 
-}
+window.addEventListener('load',()=>{
+    let tre=localStorage.getItem('true')
+    let namse=JSON.parse(localStorage.getItem('data'))
+    if(!tre){
+        btn.onclick=()=>{
+            let prompts=prompt('сколь участников')
+            localStorage.setItem('numbers',prompts)
+            if(prompts <= 1){
+                alert('участников слишком мало их долно быть больше одного')
+            }else{
+                window.open('index2.html','_self')
+            }
+        
+        }  
+    }else{
+        let pel=namse.map(item =>{
+            return `
+            <p class="num ${item.length}">${item}</p>`
+        }).join('')
+        inputs.innerHTML=pel
+        btn.innerHTML='запустить рандом'
+        btn.onclick=(e)=>{
+            e.preventDefault();
+            let random=0
+            for(let i=0;i<namse.length;i++){
+                random=Math.floor(Math.random()*namse.length)
+            }
+            alert(namse[random])
+            const pp=document.querySelectorAll('p')
+            pp[random].classList.add('bg')
+
+
+        }
+        const bynt=document.querySelector('.bunt')
+        let but=document.createElement('button')
+        let text='новые участники'
+        but.append(text)
+        bynt.append(but)
+        but.onclick=(e)=>{
+            window.location.reload()
+            localStorage.removeItem('true')
+
+            
+        }
+    }
+})
--- js/script2.js
+++ js/script2.js
@@ -13,11 +13,24 @@ fun()
 const btn =document.querySelector('.btn');
 const inp =document.querySelectorAll('.inp');
 let data=[]
-btn.onclick=()=>{
-    if(inp.value === ' '){
-        alert('gjkz gecns')
-    }else{
-        alert
-    }
 
+
+
+btn.onclick=()=>{
+    let data=[]
+    let core=null
+    for(let i=0;i<inp.length;i++){
+        if(inp[i].value === ''){
+            core=inp
+        }else{
+            data[i]=inp[i].value
+        }
+    }
+    if(core){
+        alert('поля не заполнены')
+    }else{
+        localStorage.setItem('data',JSON.stringify(data))
+    localStorage.setItem('true',true)
+    window.open('index.html','_self')
+    }
 }
